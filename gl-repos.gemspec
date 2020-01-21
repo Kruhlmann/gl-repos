1bfce97b54d4a032de57ff7826a3591d44ce8bf5
Andreas Kruhlmann <kruhlmann@protonmail.com>
2020-01-21 22:42:41 +0100

    Init

---
+++ gl-repos.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+    s.name          = 'gl-repos'
+    s.version       = '0.0.1'
+    s.date          = '2020-1-21'
+    s.summary       = "Lists GitLab repos and their corresponding URLs"
+    s.description   = ""
+    s.authors       = ["Andreas Kruhlmann"]
+    s.email         = 'kruhlmann@protonmail.com'
+    s.files         = ["lib/gl-repos/main.rb"]
+    s.homepage      = 'https://github.com/Kruhlmann/gl-repos'
+    s.license       = 'GPL-3.0-or-later'
+    s.executables   = ["gl-repos-list", "gl-repos-cache"]
+end
---
+++ lib/gl-repos/main.rb
@@ -0,0 +1,27 @@
+require "gitlab"
+
+module Fetcher
+
+  def print()
+    if not File.exist? ENV["GITLAB_REPO_CACHE"]
+      fetch()
+    end
+    File.open(ENV["GITLAB_REPO_CACHE"], "r:UTF-8") do |file_handle|
+      file_handle.each_line do |repo|
+        if repo.length > 0
+          STDOUT.write "#{repo.split(":")[0]}\n"
+        end
+      end
+    end
+  end
+
+  def fetch()
+    cache = ""
+    Gitlab.endpoint = ENV["GITLAB_ENDPOINT"] 
+    Gitlab.private_token = ENV["GITLAB_TOKEN"] 
+    Gitlab.projects.auto_paginate do |project|
+      cache="#{cache}\n#{project.name_with_namespace}:#{project.web_url}"
+    end
+    File.write(ENV["GITLAB_REPO_CACHE"], cache)
+  end
+end
